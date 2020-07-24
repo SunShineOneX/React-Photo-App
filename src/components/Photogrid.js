5a29ac09b0af35ebb412c93cdd666e989bebf337
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 11:55:55 -0400

    photo grid updates

--- src/components/Photogrid.js
+++ src/components/Photogrid.js
@@ -7,7 +7,13 @@ function Photogrid() {
             <div class="container">
                 <div class="row">
                     <div class="col">
-
+                        1 of 3
+                    </div>
+                    <div class="col-lg-6">
+                        2 of 3
+                    </div>
+                    <div class="col">
+                        3 of 3
                     </div>
                 </div>
             </div>
