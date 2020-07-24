4a92ad156c8a75216d9abecf4ac0cdcb06ea20c0
Jeff Rock <SunShineOneX@gmail.com>
2020-07-24 11:05:33 -0400

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
     return (
       <Router>
       <div >
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand">Senna</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -24,10 +24,38 @@ class Navbar extends Component {
         <a class="nav-item nav-link"><Link to="/">Home</Link></a>
         <a class="nav-item nav-link"><Link to="/UserProfile">User Profile</Link></a>
         <a class="nav-item nav-link"><Link to="/Bookmarks">Bookmarks</Link></a>
-        <a class="nav-item nav-link" id="logout">Logout</a>
+        <a class="nav-item nav-link ml-auto" id="logout">Logout</a>
       </div>
     </div>
-  </nav>
+  </nav> */}
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="">Senna</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="">Shop</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">Read</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">Stores</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="">Search</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="">Logout</a>
+            </li>
+            </ul>
+        </div>
+      </nav>
   <     Switch>
           <Route exact path="/" component={Home} />
           <Route path="/UserProfile" component={UserProfile} />
