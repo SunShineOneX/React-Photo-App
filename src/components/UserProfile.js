3fdb3acae6b5555b9bcd97fcdaaaa0cfdbb56a69
Jeff Rock <SunShineOneX@gmail.com>
2020-07-26 11:11:50 -0400

    photo style user profile upd

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -28,33 +28,30 @@ class UserProfile extends Component {
             </div>
           </div>
            */}
-
-<div class="row">
+            <div class="card-deck">
+                <div class="row">
                     <div class="col-lg-4">
                         <div id="card" class="card">
-                            <p>BIOLOGY+BEYOND</p>
+                            
                             <img class="card-images" src={require("../assets/coffee.jpg")}className="card-img-top frag" alt=""></img>
-                            <p class="sub-title">Making Sense of the Genome at Last</p>
-                            <p class="author">by Adam Piere</p>
+                           
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div id="card" class="card">
-                            <p>Women's Science & Engineering</p>
+                            
                             <img class="card-images" src={require("../assets/city.jpg")}className="card-img-top frag" alt=""></img>
-                            <p class="sub-title">Your Brain Is On the Brink of Chaos</p>
-                            <p class="author">by Kelly Clancy</p>
+                            
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div id="card" class="card">
-                            <p><strong>QUANTA</strong> ABSTRACTIONS</p>
+                            
                             <img class="card-images" src={require("../assets/laptop.jpeg")}className="card-img-top frag" alt=""></img>
-                            <p class="sub-title">Mathematicians Calculate How Randomness Creeps In</p>
-                            <p class="author">by Marcus Woo</p>
+                            
                         </div>
                     </div>
-                
+                </div>
         </div>
       </div>
     );
