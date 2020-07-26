24c748093feaa7ad41d5866b56ba9a6365723e2f
Jeff Rock <SunShineOneX@gmail.com>
2020-07-26 10:54:32 -0400

    user grid photo upd

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class UserProfile extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-                <h1 className="profile-h1">My profile</h1>
-                <img className="profile-pic" src={require("../assets/jeff2-copy.jpeg")} />
+class UserProfile extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <h1 className="profile-h1">My profile</h1>
+        <img
+          className="profile-pic"
+          src={require("../assets/jeff2-copy.jpeg")}
+        />
+        <div class="row">
+          <div class="col-lg-4">
+            <div id="card" class="card">
+                
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default UserProfile;
