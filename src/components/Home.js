38124a8bbf24594202daeb8467dcbb82f120d13a
Jeff Rock <SunShineOneX@gmail.com>
2020-07-26 12:11:18 -0400

    card deck upd

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import "../Homepage.css"
-import { BrowserRouter as Router, Redirect, Switch, Route } from  "react-router-dom";
-import { Link } from "react-router-dom"; 
+import React from "react";
+import "../Homepage.css";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Photogrid from "./Photogrid";
 import UserJeff from "./UserJeff";
 import UserSteph from "./UserSteph";
 
-
 function Home() {
   return (
-        
-            <div>
-                <div className="container-fluid" id="senna-container">
-                    <img src={require("../assets/senna-logo-white-transparent-cropped.png")}className="senna-logo" alt="" />
-                </div>
-                
-                  
-                       
-            </div>
-        
+    <div className="container-fluid">
+      <div className="container-fluid" id="senna-container">
+        <img
+          src={require("../assets/senna-logo-white-transparent-cropped.png")}
+          className="senna-logo"
+          alt=""
+        />
+      </div>
+      <div class="card-deck">
+        <div class="row">
+          <div class="col-lg-4"></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
